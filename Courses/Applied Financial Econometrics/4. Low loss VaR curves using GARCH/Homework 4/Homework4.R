da56c5d88a0804b5086688c851ebd080ab86e207
Bakeforfun <bakeforfun@gmail.com>
2015-12-12 01:12:50 +0300

    AFE HM 4 in progress

--- Courses/Applied Financial Econometrics/4. Low loss VaR curves using GARCH/Homework 4/Homework4.R
+++ Courses/Applied Financial Econometrics/4. Low loss VaR curves using GARCH/Homework 4/Homework4.R
@@ -1,5 +1,5 @@
 ##### Initialisation #####
-setwd("~/CMF/Courses/Applied Financial Econometrics/3. Portfolio modeling, multivariate GHD & Copulas/Homework 3")
+setwd("~/CMF/Courses/Applied Financial Econometrics/4. Low loss VaR curves using GARCH/Homework 4")
 library(quantmod)
 library(ggplot2)
 library(ghyp)
@@ -10,13 +10,13 @@ library(tseries)
 Sys.setlocale("LC_ALL","English")
 
 ##### Loading data #####
-# tickers = c("AAPL","GOOGL") # specify the tickers to load
-tickers = c("SPY") # specify the tickers to load
+tickers = c("SPY","DIA") # specify the tickers to load
+
 e <- new.env() # environment where data will be stored
 options("getSymbols.warning4.0"=FALSE) # suppress warning about future change in the function behaviour
-getSymbols(tickers, src = "yahoo", from="2014-01-01", env=e) # load data from Yahoo Finance
+getSymbols(tickers, src = "google", from="2014-01-01", env=e) # load data from Yahoo Finance
 
-data = do.call(merge, eapply(e, Ad)[tickers]) # merge data into a single xts taking only adjusted prices
+data = do.call(merge, eapply(e, Cl)[tickers]) # merge data into a single xts taking only adjusted prices
 names(data) = tickers
 
 ##### Calculate returns #####
@@ -25,33 +25,148 @@ rets = xts(rets, order.by = index(data)[-1])
 
 ##### LM-test #####
 ArchTest(rets$SPY,lags=12)
+ArchTest(rets$DIA,lags=12)
 
 ##### GARCH modelling #####
-rets.gfit <- garchFit(formula=~aparch(1,1),data=rets,delta=2,
+#SPY
+SPY.gfit <- garchFit(formula=~aparch(1,1),data=rets$SPY,delta=2,
                      include.delta=FALSE,leverage=TRUE,cond.dist="sged",
-                     shape=1.6,include.shape=FALSE,trace=FALSE)
+                     shape=1.2,include.shape=TRUE,trace=FALSE)
 
-plot(rets.gfit,which=13)
-plot(rets.gfit,which=10)
+plot(SPY.gfit,which=13)
+plot(SPY.gfit,which=10)
 
-##### ADF-test #####
-adf.test(rets)
+#DIA
+DIA.gfit <- garchFit(formula=~aparch(1,1),data=rets$DIA,delta=2,
+                     include.delta=FALSE,leverage=TRUE,cond.dist="sged",
+                     shape=1.2,include.shape=TRUE,trace=FALSE)
 
-##### PP-test #####
-pp.test(rets)
+plot(DIA.gfit,which=13)
+plot(DIA.gfit,which=10)
 
-##### KPSS-test #####
-kpss.test(rets, null="Level")
+##### Stationarity tests #####
+adf.test(rets$SPY)
+adf.test(rets$DIA)
 
-# прогноз среднего и дисперсии на i шагов вперёд
-i = 1
-rets.frc <- predict(rets.gfit,n.ahead=i)
-rets.frc[,1] # вектор средних
-rets.frc[,3]^2 # вектор дисперсий
+pp.test(rets$SPY)
+pp.test(rets$DIA)
 
-# расчёт границы потерь
-alpha <- 0.05
-VaR <- rets.frc[1,1]+rets.frc[1,3]*qged(alpha,mean=0,sd=1, nu=rets.gfit@fit$par["shape"])
+kpss.test(rets$SPY, null="Level")
+kpss.test(rets$DIA, null="Level")
+
+##### Standardized residuals #####
+z <- matrix(nrow=length(rets$SPY),ncol=2)
+z[,1] <- SPY.gfit@residuals / SPY.gfit@sigma.t
+z[,2] <- DIA.gfit@residuals / DIA.gfit@sigma.t
+
+##### Residuals partial distributions #####
+mean <- c(0,0); sd <- c(1,1); nu <- c(1.2,1.2)
+
+xi <- c(SPY.gfit@fit$par["skew"], DIA.gfit@fit$par["skew"])
+cdf <- matrix(nrow=length(rets$SPY),ncol=2)
+for (i in 1:2) cdf[,i] = psged(z[,i],mean=mean[i],sd=sd[i],nu=nu[i],xi=xi[i])
+
+##### Initialise copulas #####
+norm.cop <- normalCopula(dim=2,param=0.5,dispstr="un")
+stud.cop <- tCopula(dim=2,param=0.5,df=5,df.fixed=TRUE,dispstr="un")
+gumb.cop <- gumbelCopula(dim=2,param=2)
+clay.cop <- claytonCopula(dim=2,param=2)
+
+##### Fit copulas #####
+norm.fit <- fitCopula(cdf,copula=norm.cop)
+stud.fit <- fitCopula(cdf,copula=stud.cop)
+gumb.fit <- fitCopula(cdf,copula=gumb.cop)
+clay.fit <- fitCopula(cdf,copula=clay.cop)
+
+##### Monte-Carlo
+N = 100000
+cdf.sim <- rcopula(n=N,copula=stud.fit@copula)
+z.sim <- matrix(nrow=N,ncol=2)
+for (i in 1:2) z.sim[,i] <- qsged(cdf.sim[,i],
+                                  mean=mean[i],sd=sd[i],nu=nu[i],xi=xi[i])
+frc1 <- predict(SPY.gfit,n.ahead=1)
+frc2 <- predict(DIA.gfit,n.ahead=1)
+mu <- c(frc1[,1],frc2[,1])
+sigma <- c(frc1[,3],frc2[,3])
+
+##### Modelled portfolio returns #####
+w = c(0.5, 0,5)
+prt.sim <- w[1]*(mu[1]+sigma[1]*z.sim[,1]) +
+  w[2]*(mu[2]+sigma[2]*z.sim[,2])
+
+##### VaR and ES #####
+alpha <- 0.10
+prt.sim <- sort(prt.sim)
+VaR <- prt.sim[alpha*N]
+ES <- mean(prt.sim[1:(alpha*N-1)])
+
+##### Calculate VaR curve #####
+VaR_curve <- numeric()
+h <- 0.5 * 260 # window length
+
+for (i in (h+1):length(rets[,1])) 
+{
+  h.rets <- rets[(i-h):(i-1),]
+
+  SPY.gfit <- garchFit(formula=~aparch(1,1),data=h.rets$SPY,delta=2,
+                       include.delta=FALSE,leverage=TRUE,cond.dist="sged",
+                       shape=1.2,include.shape=TRUE,trace=FALSE)
+  
+  DIA.gfit <- garchFit(formula=~aparch(1,1),data=h.rets$DIA,delta=2,
+                       include.delta=FALSE,leverage=TRUE,cond.dist="sged",
+                       shape=1.2,include.shape=TRUE,trace=FALSE)
+  
+  z <- matrix(nrow=length(h.rets$SPY),ncol=2)
+  z[,1] <- SPY.gfit@residuals / SPY.gfit@sigma.t
+  z[,2] <- DIA.gfit@residuals / DIA.gfit@sigma.t
+  
+  mean <- c(0,0); sd <- c(1,1); nu <- c(1.2,1.2)
+  xi <- c(SPY.gfit@fit$par["skew"], DIA.gfit@fit$par["skew"])
+  cdf <- matrix(nrow=length(h.rets$SPY),ncol=2)
+  for (j in 1:2) cdf[,j] = psged(z[,j],mean=mean[j],sd=sd[j],nu=nu[j],xi=xi[j])
+  
+  stud.fit <- fitCopula(cdf,copula=stud.cop)
+  
+  N = 100000
+  cdf.sim <- rcopula(n=N,copula=stud.fit@copula)
+  z.sim <- matrix(nrow=N,ncol=2)
+  for (j in 1:2) z.sim[,j] <- qsged(cdf.sim[,j],
+                                    mean=mean[j],sd=sd[j],nu=nu[j],xi=xi[j])
+  frc1 <- predict(SPY.gfit,n.ahead=1)
+  frc2 <- predict(DIA.gfit,n.ahead=1)
+  h.mu <- c(frc1[,1],frc2[,1])
+  h.sigma <- c(frc1[,3],frc2[,3])
+  
+  h.prt.sim <- w[1]*(h.mu[1]+h.sigma[1]*z.sim[,1]) +
+    w[2]*(h.mu[2]+h.sigma[2]*z.sim[,2])
+  
+  h.prt.sim <- sort(prt.sim)
+  
+  VaR_curve[i-h] <- h.prt.sim[alpha*N]
+}
+
+fact <- prt.sim[(h+1):length(prt.sim)] # FFFIIIIIINIIIIISSHHHHH
+plot(as.numeric(fact),type="l", ylab = "Return")
+lines(VaR_curve,col="red")
+
+##### Kupiec test #####
+K <- sum(fact<VaR_curve); alpha0 <- K/length(rets)
+S <- -2*log((1-alpha)^(length(rets)-K)*alpha^K)+
+  2*log((1-alpha0)^(length(rets)-K)*alpha0^K)
+p.value <- 1-pchisq(S,df=1)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 ##### VaR curve #####
@@ -128,82 +243,6 @@ for (i in (h+1):length(prt[,1]))
 
   sim <- rghyp(n=N,object=h.prt.fit)
   h.prt.sim <- h.w[i-h,1]*sim[,1]+h.w[i-h,2]*sim[,2]
-  h.prt.sim <- sort(h.prt.sim)
-  VaR_curve[i-h] <- h.prt.sim[alpha*N]
-}
-
-fact <- prt[(h+1):length(prt[,1]),1]*h.w[,1] + prt[(h+1):length(prt[,2]),2]*h.w[,2]
-plot(as.numeric(fact),type="l", ylab = "Return")
-lines(VaR_curve,col="red")
-
-##### Kupiec test #####
-K <- sum(fact<VaR_curve); alpha0 <- K/length(rets)
-S <- -2*log((1-alpha)^(length(rets)-K)*alpha^K)+
-  2*log((1-alpha0)^(length(rets)-K)*alpha0^K)
-p.value <- 1-pchisq(S,df=1)
-
-##### Fit assets' distributions #####
-aapl.fit <- stepAIC.ghyp(rets$AAPL,dist=c("gauss","t","ghyp"),
-                        symmetric=NULL,silent=TRUE)$best.model
-googl.fit <- stepAIC.ghyp(rets$GOOGL,dist=c("gauss","t","ghyp"),
-                        symmetric=NULL,silent=TRUE)$best.model
-
-aapl.cdf <- pghyp(rets$AAPL,object=aapl.fit)
-googl.cdf <- pghyp(rets$GOOGL,object=googl.fit)
-cdf <- cbind(as.numeric(aapl.cdf), as.numeric(googl.cdf))
-
-##### Initialise copulas #####
-norm.cop <- normalCopula(dim=2,param=0.5,dispstr="un")
-stud.cop <- tCopula(dim=2,param=0.5,df=5,df.fixed=TRUE,dispstr="un")
-gumb.cop <- gumbelCopula(dim=2,param=2)
-clay.cop <- claytonCopula(dim=2,param=2)
-
-##### Fit copulas #####
-norm.fit <- fitCopula(cdf,copula=norm.cop)
-stud.fit <- fitCopula(cdf,copula=stud.cop)
-gumb.fit <- fitCopula(cdf,copula=gumb.cop)
-clay.fit <- fitCopula(cdf,copula=clay.cop)
-
-##### Get assets' returns #####
-N <- 10^4
-stud.sim <- rCopula(n=N,copula=stud.fit@copula)
-
-aapl.sim <- qghyp(stud.sim[,1],object=aapl.fit)
-googl.sim <- qghyp(stud.sim[,2],object=googl.fit)
-w_copula <- c(0.5,0.5)
-prt.sim <- w_copula[1]*aapl.sim + w_copula[2]*googl.sim
-
-##### VaR and ES #####
-alpha <- 0.10
-prt.sim <- sort(prt.sim)
-VaR <- prt.sim[alpha*N]
-ES <- mean(prt.sim[1:(alpha*N-1)])
-
-##### Calculate VaR curve #####
-VaR_curve <- numeric()
-h <- 0.5 * 260 # window length
-
-
-for (i in (h+1):length(rets[,1])) 
-{
-  h.rets <- rets[(i-h):(i-1),]
-  
-  aapl.fit <- stepAIC.ghyp(h.rets$AAPL,dist=c("t","ghyp"),
-                           symmetric=NULL,silent=TRUE)$best.model
-  googl.fit <- stepAIC.ghyp(h.rets$GOOGL,dist=c("t","ghyp"),
-                            symmetric=NULL,silent=TRUE)$best.model
-  
-  aapl.cdf <- pghyp(h.rets$AAPL,object=aapl.fit)
-  googl.cdf <- pghyp(h.rets$GOOGL,object=googl.fit)
-  cdf <- cbind(as.numeric(aapl.cdf), as.numeric(googl.cdf))
-  
-  stud.fit <- fitCopula(cdf,copula=stud.cop)
-  
-  aapl.sim <- qghyp(stud.sim[,1],object=aapl.fit)
-  googl.sim <- qghyp(stud.sim[,2],object=googl.fit)
-  
-  h.prt.sim <- w[1]*aapl.sim + w[2]*googl.sim
-  
   h.prt.sim <- sort(h.prt.sim)
   VaR_curve[i-h] <- h.prt.sim[alpha*N]
 }
