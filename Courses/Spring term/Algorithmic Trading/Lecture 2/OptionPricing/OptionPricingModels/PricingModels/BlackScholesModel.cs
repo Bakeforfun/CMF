71b9c8aa323f60451d764c90757240114cb54a33
Bakeforfun <bakeforfun@gmail.com>
2016-02-25 19:35:37 +0300

    L2 added, Black-Scholes implemented

--- Courses/Spring term/Algorithmic Trading/Lecture 2/OptionPricing/OptionPricingModels/PricingModels/BlackScholesModel.cs
+++ Courses/Spring term/Algorithmic Trading/Lecture 2/OptionPricing/OptionPricingModels/PricingModels/BlackScholesModel.cs
@@ -1,13 +1,15 @@
 ﻿// (c) Konstantin Brownstein 2016, except implementation
 
 using BaseEntities;
+using OptionPricingModels.Services;
+using System;
 
 namespace OptionPricingModels.PricingModels {
     internal class BlackScholesModel : IOptionsPricingModel {
         public void Compute(OptionPosition option) {
-            var t = option.TimeToExpiry.Days/365.0;
-            // var d1 = 
-            // var d2 = 
+            var t = option.TimeToExpiry.Days / 365.0;
+            var d1 = (Math.Log(option.UnderlyingPrice / option.Strike) + (option.InterestRate + option.Volatility * option.Volatility / 2) * t) / (option.Volatility * Math.Sqrt(t));
+            var d2 = d1 - option.Volatility * Math.Sqrt(t);
 
             var sign = option.Side == Side.Buy ? 1 : -1;
             // when calculating option greeks keep in mind that we calculate greek for position, 
@@ -15,14 +17,22 @@ namespace OptionPricingModels.PricingModels {
             // so formulas for greeks are [sign * BSFormula]
 
             if (option.Type == OptionType.Call) {
-                // option.OptionPrice = 
-                //option.Delta = 
-                //option.Theta = 
-                //option.Rho = 
+                option.OptionPrice = option.UnderlyingPrice * NormalDistribution.Phi(d1) - option.Strike * Math.Pow(Math.E, -option.InterestRate * t) * NormalDistribution.Phi(d2);
+                option.Delta = sign * NormalDistribution.Phi(d1);
+                option.Theta = sign * (-(option.UnderlyingPrice * NormalDistribution.Density(d1) * option.Volatility) / (2 * Math.Sqrt(t)) -
+                                option.InterestRate * option.Strike * Math.Pow(Math.E, -option.InterestRate * t) * NormalDistribution.Phi(d2));
+                option.Rho = sign * option.Strike * t * Math.Pow(Math.E, -option.InterestRate * t) * NormalDistribution.Phi(d2);
+            }
+            else {
+                option.OptionPrice = -option.UnderlyingPrice * NormalDistribution.Phi(-d1) + option.Strike * Math.Pow(Math.E, -option.InterestRate * t) * NormalDistribution.Phi(-d2);
+                option.Delta = sign * (NormalDistribution.Phi(d1) - 1);
+                option.Theta = sign * (-(option.UnderlyingPrice * NormalDistribution.Density(d1) * option.Volatility) / (2 * Math.Sqrt(t)) +
+                                option.InterestRate * option.Strike * Math.Pow(Math.E, -option.InterestRate * t) * NormalDistribution.Phi(-d2));
+                option.Rho = sign * (-1) * option.Strike * t * Math.Pow(Math.E, -option.InterestRate * t) * NormalDistribution.Phi(-d2);
             }
 
-            //option.Gamma = 
-            //option.Vega = 
+            option.Gamma = sign * NormalDistribution.Density(d1) / (option.UnderlyingPrice * option.Volatility * Math.Sqrt(t));
+            option.Vega = sign * option.UnderlyingPrice * Math.Sqrt(t) * NormalDistribution.Density(d1);
         }
     }
 }
