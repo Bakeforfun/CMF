22927acf007f82993b1136ebac0b4dcba1189477
Bakeforfun <bakeforfun@gmail.com>
2016-03-01 19:14:44 +0300

    Binomial model - done

--- Courses/Spring term/Algorithmic Trading/Lecture 2/OptionPricing/OptionPricingModels/PricingModels/BinomialModel.cs
+++ Courses/Spring term/Algorithmic Trading/Lecture 2/OptionPricing/OptionPricingModels/PricingModels/BinomialModel.cs
@@ -1,21 +1,56 @@
 ﻿// (c) Konstantin Brownstein 2016, except implementation
 
+using BaseEntities;
 using System;
 
 namespace OptionPricingModels.PricingModels {
     internal class BinomialModel : IOptionsPricingModel {
         public void Compute(OptionPosition option) {
-            int nSteps = 10; // number of steps in the binomial model
+            int nSteps = 1000; // number of steps in the binomial model
 
-            var t = option.TimeToExpiry.Days / 365.0;
+            double t = option.TimeToExpiry.Days / 365.0;
             double dt = t / nSteps; // time interval between steps
 
-            double upFactor = Math.Pow(Math.E, option.Volatility * Math.Sqrt(dt));
-            double downFactor = 1 / upFactor;
-            double p = (Math.Pow(Math.E, option.InterestRate * dt) - downFactor) / (upFactor - downFactor); // probability of an up-move
+            double S = option.UnderlyingPrice;
+            double K = option.Strike;
 
-            double[, ] underlyingPrice = new double[nSteps + 1, nSteps + 1]; // an array containing underlying prices of the underlying at each step
-            underlyingPrice[0, 0] = option.UnderlyingPrice; // price at the root of the tree is equal to the current price
+            double sigma = option.Volatility;
+            double r = option.InterestRate;
+
+            OptionType type = option.Type;
+
+            double[,] lattice = createLattice(nSteps, dt, S, K, sigma, r, type);
+
+            var sign = option.Side == Side.Buy ? 1 : -1;
+
+            double upFactor = Math.Pow(Math.E, sigma * Math.Sqrt(dt));
+            double downFactor = 1 / upFactor;
+
+            option.OptionPrice = lattice[0, 0];
+
+            option.Delta = sign * (lattice[1, 1] - lattice[1, 0]) / (option.UnderlyingPrice * (upFactor - downFactor));
+
+            option.Gamma = sign * (((lattice[2, 2] - lattice[2, 1]) / (option.UnderlyingPrice * (upFactor * upFactor - upFactor * downFactor))) -
+                                   ((lattice[2, 1] - lattice[2, 0]) / (option.UnderlyingPrice * (upFactor * downFactor - downFactor * downFactor)))) /
+                                    (0.5 * option.UnderlyingPrice * (upFactor * upFactor - downFactor * downFactor)); 
+
+            option.Theta = sign * (lattice[2, 1] - lattice[0, 0]) / (2 * dt);
+
+            double priceDeltaVol = createLattice(nSteps, dt, S, K, (sigma + 0.001), r, type)[0, 0]; // option price with a higher volatily for vega calculation
+            option.Vega = sign * (priceDeltaVol - option.OptionPrice) / 0.001; // estimate option price sensitivity to volatility
+
+            double priceDeltaR = createLattice(nSteps, dt, S, K, sigma, (r + 0.001), type)[0, 0]; // option price with a higher rate for rho calculation
+            option.Rho = sign * (priceDeltaR - option.OptionPrice) / 0.001; // estimate option price sensitivity to percent rate
+        }
+
+        private static double[,] createLattice(int nSteps, double dt, double S, double K, double sigma, double r, OptionType type)
+        {
+            double upFactor = Math.Pow(Math.E, sigma * Math.Sqrt(dt));
+            double downFactor = 1 / upFactor;
+            double p = (Math.Pow(Math.E, r * dt) - downFactor) / (upFactor - downFactor); // probability of an up-move
+
+            double[,] underlyingPrice = new double[nSteps + 1, nSteps + 1]; // an array containing underlying prices of the underlying at each step
+            underlyingPrice[0, 0] = S; // price at the root of the tree is equal to the current price
             underlyingPrice[nSteps, 0] = underlyingPrice[0, 0] * Math.Pow(downFactor, nSteps);
 
             for (int i = 1; i <= nSteps; i++) // initialise asset prices at maturity
@@ -23,17 +58,17 @@ namespace OptionPricingModels.PricingModels {
                 underlyingPrice[nSteps, i] = underlyingPrice[nSteps, i - 1] * upFactor / downFactor;
             }
 
-            double[, ] optionPrice = new double[nSteps + 1, nSteps + 1]; // an array containing option prices of the underlying at each step
+            double[,] lattice = new double[nSteps + 1, nSteps + 1]; // an array containing option prices of the underlying at each step
 
             for (int i = 0; i <= nSteps; i++) // initialise option prices at maturity
             {
-                if (option.Type == OptionType.Call)
+                if (type == OptionType.Call)
                 {
-                    optionPrice[nSteps, i] = Math.Max(underlyingPrice[nSteps, i] - option.Strike, 0);
+                    lattice[nSteps, i] = Math.Max(underlyingPrice[nSteps, i] - K, 0);
                 }
                 else
                 {
-                    optionPrice[nSteps, i] = Math.Max(option.Strike - underlyingPrice[nSteps, i], 0);
+                    lattice[nSteps, i] = Math.Max(K - underlyingPrice[nSteps, i], 0);
                 }
             }
 
@@ -41,16 +76,11 @@ namespace OptionPricingModels.PricingModels {
             {
                 for (int j = 0; j <= i; j++)
                 {
-                    optionPrice[i, j] = Math.Pow(Math.E, -option.InterestRate * dt) * (p * optionPrice[i + 1, j + 1] + (1 - p) * optionPrice[i + 1, j]);
+                    lattice[i, j] = Math.Pow(Math.E, -r * dt) * (p * lattice[i + 1, j + 1] + (1 - p) * lattice[i + 1, j]);
                 }
             }
 
-            option.OptionPrice = optionPrice[0, 0];
-            //option.Delta = 
-            //option.Gamma = 
-            //option.Vega = 
-            //option.Theta = 
-            //option.Rho = 
+            return lattice;
         }
     }
 }
